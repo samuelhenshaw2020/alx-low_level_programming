ef0295b2a1eb9a9d2c08aac463c9fe75915880bd
samuelhenshaw2020 <samuelhenshaw2020@gmail.com>
2022-09-10 05:05:49 -0700

    fixed 4,5,6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
 int main(){
- pringf("with proper grammar, but the outcome is a piece of art,\n");
+ printf("with proper grammar, but the outcome is a piece of art,\n");
  return 0;
 }
