5c21293d5801f35ddaa65e43942fadd747a40d20
samuelhenshaw2020 <samuelhenshaw2020@gmail.com>
2022-09-10 05:02:11 -0700

    fixed -m32 issue on 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
 int main(){
- pringf("with proper grammar, but the outcome is a piece of art,");
+ pringf("with proper grammar, but the outcome is a piece of art,\n");
  return 0;
 }
